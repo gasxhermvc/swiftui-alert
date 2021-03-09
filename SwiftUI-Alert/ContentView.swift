78c32a6bac219869063765310ce989e4e7804d7e
Tanasit Auewongaree <006006@cdg.co.th>
2021-03-09 16:30:38 +0700

    change display add background color

--- SwiftUI-Alert/ContentView.swift
+++ SwiftUI-Alert/ContentView.swift
@@ -14,35 +14,47 @@ struct ContentView: View {
     @State private var answer: Int = 0
     
     var body: some View {
-        VStack (spacing: 10){
-            Text("Who is the first president of america?")
-                .font(.system(size: 17, weight: .bold,design: .default))
+        ZStack {
+           BackgroundColorView()
             
-            HStack(spacing: 10) {
-                Button {
-                    showMessage.toggle()
-                    self.answer = 1
-                } label: {
-                    Text("John Adams")
-                        .font(.system(size: 14,weight: .medium))
-                        .frame(width: 150, height: 50)
-                        .background(Color.red)
-                        .foregroundColor(.white)
-                }
+            VStack (spacing: 10){
+                Spacer()
                 
-                Button {
-                    showMessage.toggle()
-                    self.answer = 2
-                } label: {
-                    Text("George Washington")
-                        .font(.system(size: 14, weight: .medium))
-                        .frame(width: 150, height: 50)
-                        .background(Color.blue)
-                        .foregroundColor(.white)
-                }
-            }.alert(isPresented: $showMessage, content: {
-                alertMessage(self.answer)
-            })
+                BackgroundView()
+                
+                Text("Who is the first president of america?")
+                    .font(.system(size: 17, weight: .bold,design: .default))
+                
+                HStack(spacing: 10) {
+                    Button {
+                        showMessage.toggle()
+                        self.answer = 1
+                    } label: {
+                        Text("John Adams")
+                            .font(.system(size: 14,weight: .medium))
+                            .frame(width: 150, height: 50)
+                            .background(Color.red)
+                            .foregroundColor(.white)
+                    }
+                    
+                    Button {
+                        showMessage.toggle()
+                        self.answer = 2
+                    } label: {
+                        Text("George Washington")
+                            .font(.system(size: 14, weight: .medium))
+                            .frame(width: 150, height: 50)
+                            .background(Color.blue)
+                            .foregroundColor(.white)
+                    }
+                }.alert(isPresented: $showMessage, content: {
+                    alertMessage(self.answer)
+                })
+                
+                Spacer()
+            }
+            .padding(10)
+            .frame(maxWidth: .infinity)
         }
     }
     
@@ -58,5 +70,23 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct BackgroundView: View{
+    var body: some View{
+        Image("swift_logo")
+            .resizable()
+            .frame(width: 200, height: 200)
+            .aspectRatio(contentMode: .fit)
+            .padding(.bottom,20)
+    }
+}
+
+
+struct BackgroundColorView: View {
+    var body: some View {
+        RadialGradient(gradient: Gradient(colors: [Color("yellow"),Color("sky")]),center: .center,startRadius: 10,endRadius: 550)
+            .edgesIgnoringSafeArea(.all)
     }
 }
