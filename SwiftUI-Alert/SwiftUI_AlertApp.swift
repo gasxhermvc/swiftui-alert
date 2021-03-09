49b66dcafdc2b4e33fe032f440d63139e59bb540
Tanasit Auewongaree <006006@cdg.co.th>
2021-03-09 15:05:49 +0700

    Initial Commit

---
+++ SwiftUI-Alert/SwiftUI_AlertApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_AlertApp.swift
+//  SwiftUI-Alert
+//
+//  Created by BillY-MAC on 9/3/2564 BE.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_AlertApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
