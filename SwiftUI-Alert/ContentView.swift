9520b2f0013bb8791b379f70eaea8d56d510fbc5
Tanasit Auewongaree <006006@cdg.co.th>
2021-03-09 15:52:18 +0700

    SwiftUI ALert

--- SwiftUI-Alert/ContentView.swift
+++ SwiftUI-Alert/ContentView.swift
@@ -8,9 +8,50 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var showMessage: Bool = false
+    
+    @State private var answer: Int = 0
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack (spacing: 10){
+            Text("Who is the first president of america?")
+                .font(.system(size: 17, weight: .bold,design: .default))
+            
+            HStack(spacing: 10) {
+                Button {
+                    showMessage.toggle()
+                    self.answer = 1
+                } label: {
+                    Text("John Adams")
+                        .font(.system(size: 14,weight: .medium))
+                        .frame(width: 150, height: 50)
+                        .background(Color.red)
+                        .foregroundColor(.white)
+                }
+                
+                Button {
+                    showMessage.toggle()
+                    self.answer = 2
+                } label: {
+                    Text("George Washington")
+                        .font(.system(size: 14, weight: .medium))
+                        .frame(width: 150, height: 50)
+                        .background(Color.blue)
+                        .foregroundColor(.white)
+                }
+            }.alert(isPresented: $showMessage, content: {
+                alertMessage(self.answer)
+            })
+        }
+    }
+    
+    func alertMessage(_ number: Int) -> Alert
+    {
+        
+        let message: String = number == 1 ? "Wrong, Looser" : "Correct!!!"
+        
+        return Alert(title: Text("Your Answer"),message: Text(message),dismissButton: .default(Text("OK")))
     }
 }
 
